8007b8e102f52d4af4e79d9363529cdaeb82fdeb
ElaineChoo <elainechoo85@gmail.com>
2018-05-16 06:20:45 +0800

    Login form with client-side validation (with error when field is empty)

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -1,11 +1,24 @@
 /*eslint-disable*/
 import React from 'react';
-import { Link } from 'react-router-dom';
+import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => (
-    <div>
-        <h1>LoginPage</h1>
-    </div>
-);
+class LoginPage extends React.Component {
+
+    submit = data => {
+        console.log(data);
+    };
+    
+    render() {
+        return (
+            <div>
+                <h1>LoginPage</h1>
+
+                <LoginForm submit={this.submit} />
+            </div>
+
+        )
+    }
+}
+
 
 export default LoginPage;
